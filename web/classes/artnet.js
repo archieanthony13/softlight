ecedab8c1358d90b0acab9ddc1fde561bd6e8d55
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2025-09-12 14:29:48 +0100

    Updated functions

--- web/classes/artnet.js
+++ web/classes/artnet.js
@@ -4,23 +4,23 @@ class Artnet{
         this.socket = new WebSocket("ws://localhost:8765");
         this.open = false
         var that = this
-        this.socket.onopen = () => { 
-            console.log("Connected to server");
+        this.socket.onopen = function(e){
+            console.log("Connected to server")
             that.open = true
-        }; 
-        
-        this.socket.onmessage = (event) => { 
-            console.log("Message from server:", event.data); 
-        }; 
-        
-        this.socket.onerror = (error) => { 
-            console.error("WebSocket error:", error); 
-        }; 
-        
-        this.socket.onclose = () => { 
-            console.log("Connection closed"); 
+        }
+
+        this.socket.onmessage = function(e){
+            console.log("Message from server:", e.data)
+        }
+
+        this.socket.onerror = function(e){
+            console.error("WebSocket error:", e)
+        }
+
+        this.socket.onclose = function(e){
+            console.log("Connection closed")
             that.open = false
-        };
+        }
     }
 
     changeIp(ip){
