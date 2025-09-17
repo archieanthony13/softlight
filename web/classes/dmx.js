4a4cbf9fc4e242b89f019a94657eaaf61760554e
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2025-09-17 09:25:45 +0100

    Created updateChannel function for DMX class

--- web/classes/dmx.js
+++ web/classes/dmx.js
@@ -2,4 +2,10 @@ class DMX{
     constructor(){
         this.data = new Array(512).fill(0)
     }
+
+    updateChannel(channel, data){
+        for(let i=0;i<data.length;i++){
+            this.data[channel - 1 + i] = data[i]
+        }
+    }
 }
