e8b4ee981f8d2bdbcf97d7b6fa1eb11fc33ad686
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2025-10-17 08:43:52 +0000

    You can upload fixtures from files and they are added to the fixture library which gets stored in localStorage so whenever the page is loaded the data is not lost

--- web/classes/fixturemanager.js
+++ web/classes/fixturemanager.js
@@ -1,5 +1,28 @@
 class FixtureManager{
     constructor(){
-        
+        this.fixtureLibrary = []
+    }
+
+    loadFixtureLibrary(){
+        if(localStorage.getItem("softlight-fixture-library")){
+            this.fixtureLibrary = JSON.parse(localStorage.getItem("softlight-fixture-library"))
+        } else {
+            this.fixtureLibrary = []
+        }
+    }
+
+    loadFixtureFromFile(){
+        let object = document.createElement('input')
+        object.type = "file"
+        object.click()
+        let that = this
+        object.onchange = function(){
+            let filereader = new FileReader()
+            filereader.onload = async function(){
+                that.fixtureLibrary.push(JSON.parse(filereader.result))
+                localStorage.setItem("softlight-fixture-library", JSON.stringify(that.fixtureLibrary))
+            }
+            filereader.readAsText(object.files[0])
+        }
     }
 }
