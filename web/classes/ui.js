29d5c1ed75f904d7bffcf44f434b853db038d74a
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2025-10-18 16:41:05 +0100

    Adds checkbox

--- web/classes/ui.js
+++ web/classes/ui.js
@@ -6,9 +6,16 @@ class UI{
     updateFixtureList(){
         document.getElementById('fixtures').innerHTML = ""
         for(let i=0;i<fixtureManager.fixtures.length;i++){
-            let element = document.createElement('p')
-            element.innerHTML = fixtureManager.fixtures[i].name
-            document.getElementById('fixtures').append(element)
+            let label = document.createElement('label')
+            label.innerHTML = fixtureManager.fixtures[i].name
+            let forAttribute = document.createAttribute('for')
+            forAttribute.value = "fixture-" + i
+            label.attributes.setNamedItem(forAttribute)
+            let input = document.createElement('input')
+            input.type = "checkbox"
+            input.name = "fixture-" + i
+            document.getElementById('fixtures').append(input)
+            document.getElementById('fixtures').append(label)
         }
     }
 }
