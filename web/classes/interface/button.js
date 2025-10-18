2bd4a1ad99d30fd79b252c6d991dd8bae1b62127
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2025-10-18 11:37:22 +0100

    Select fixtures from a list

--- web/classes/interface/button.js
+++ web/classes/interface/button.js
@@ -7,14 +7,24 @@ class Button{
         this.onclick = onclick
         this.display = display
         this.color = color
+        this.holdable = true
+        this.active = false
+    }
+
+    setHoldable(value){
+        this.holdable = value
     }
 
     update(){
         if(this.display){
             renderManager.renderColor(this.color,this.x,this.y,this.w,this.h)
             let mouse = inputManager.getMouse()
-            if(mouse.pos[0] >= this.x && mouse.pos[0] <= this.x + this.w && mouse.pos[1] >= this.y && mouse.pos[1] <= this.y + this.h && mouse.click){
+            if(mouse.pos[0] >= this.x && mouse.pos[0] <= this.x + this.w && mouse.pos[1] >= this.y && mouse.pos[1] <= this.y + this.h && mouse.click && ((!this.holdable && !this.active) || this.holdable)){
+                this.active = true
                 this.onclick()
+            }
+            if(mouse.click == false){
+                this.active = false
             }
         }
     }
