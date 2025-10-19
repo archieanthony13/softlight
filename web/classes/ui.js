d3bc0d52a19f4f811d91722857ffb343ad61daf8
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2025-10-19 17:57:12 +0100

    Fixed bug with updating values

--- web/classes/ui.js
+++ web/classes/ui.js
@@ -10,16 +10,15 @@ class UI{
             }
         })
 
-        document.getElementById('attribute-values').onchange = function(e){
-            let labels = document.querySelectorAll("#attribute-values label")
-            let inputs = document.querySelectorAll("#attribute-values input")
-            for(let i=0;i<labels.length;i++){
-                if(labels[i].innerHTML != ""){
-                    console.log(labels[i].innerHTML, inputs[i].value)
-                    fixtureManager.updateSelectedFixtureChannel(labels[i].innerHTML, parseInt(inputs[i].value))
+        Array.from(document.querySelectorAll('#attribute-values input')).forEach(function(n){
+            n.onchange = function(){
+                let label = document.querySelector("#attribute-values label#" + n.id + "-label")
+                let input = n
+                if(label.innerHTML != ""){
+                    fixtureManager.updateSelectedFixtureChannel(label.innerHTML, parseInt(input.value))
                 }
             }
-        }
+        })
     }
 
     updateFixtureList(){
