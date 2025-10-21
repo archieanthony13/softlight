7d79e70210eee1c8a66ce8b2165e6b1670eff0ae
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2025-10-21 12:30:46 +0100

    Created patch menu functions but there is an error with running the .onchange function as the element does not yet exist

--- web/classes/patch.js
+++ web/classes/patch.js
@@ -1,7 +1,21 @@
 class Patch{
     constructor(){
         this.patchElement = document.querySelector('.patch-menu')
+        this.patchElementBottom = this.patchElement.querySelector(".patch-menu-bottom-section")
         this.active = false
+
+        let that = this
+        this.patchElement.querySelector("#patch-fixture").onclick = function(){
+            that.patchFixtureMenu()
+        }
+
+        this.patchElementBottom.querySelector("#manufacturer").onchange = function(){
+            that.updateFixtureNameSelect()
+            that.updateChannelSelect()
+        }
+        this.patchElementBottom.querySelector("#name").onchange = function(){
+            that.updateChannelSelect()
+        }
     }
 
     togglePatchMenu(){
@@ -15,7 +29,7 @@ class Patch{
     }
 
     updateFixtureList(){
-        this.patchElement.querySelector(".patch-menu-bottom-section").innerHTML = ""
+        this.patchElementBottom.innerHTML = ""
         for(let i=0;i<fixtureManager.fixtures.length;i++){
             let label = document.createElement('label')
             label.innerHTML = fixtureManager.fixtures[i].name
@@ -27,9 +41,71 @@ class Patch{
             input.name = "patch-" + i
             input.id = "patch-" + i
             input.hidden = true
-            this.patchElement.querySelector(".patch-menu-bottom-section").append(input)
-            this.patchElement.querySelector(".patch-menu-bottom-section").append(label)
+            this.patchElementBottom.append(input)
+            this.patchElementBottom.append(label)
         }
-        this.patchElement.querySelector(".patch-menu-bottom-section").style.gridTemplateRows = "repeat(" + fixtureManager.fixtures.length + ", calc(var(--scale)*4))"
+        this.patchElementBottom.style.gridTemplateRows = "repeat(" + fixtureManager.fixtures.length + ", calc(var(--scale)*4))"
+    }
+
+    patchFixtureMenu(){
+        this.patchElementBottom.append(manufacturerElement)
+        this.patchElementBottom.append(nameElement)
+        this.patchElementBottom.append(channelElement)
+        this.updateManufacturerSelect()
+        this.updateFixtureNameSelect()
+        this.updateChannelSelect()
+    }
+
+    updateManufacturerSelect(){
+        let manufacturerElement = this.patchElementBottom.querySelector("#manufacturer")
+        let manufacturersList = []
+        manufacturerElement.innerHTML = ""
+        for(let i=0;i<fixtureManager.fixtureLibrary.length;i++){
+            let fixture = fixtureManager.fixtureLibrary[i]
+            let manufacturer = fixture.manufacturer
+            if(manufacturersList.indexOf(manufacturer) == -1){
+                let option = document.createElement('option')
+                option.value = manufacturer
+                option.innerHTML = manufacturer
+                manufacturerElement.append(option)
+                manufacturersList.push(manufacturer)
+            }
+        }
+    }
+
+    updateFixtureNameSelect(){
+        let manufacturer = this.patchElementBottom.querySelector("#manufacturer").value
+        let nameElement = this.patchElementBottom.querySelector("#name")
+        nameElement.innerHTML = ""
+        for(let i=0;i<fixtureManager.fixtureLibrary.length;i++){
+            let fixture = fixtureManager.fixtureLibrary[i]
+            if(fixture.manufacturer == manufacturer){
+                let name = fixture.name
+                let nameOption = document.createElement('option')
+                nameOption.value = name
+                nameOption.innerHTML = name
+                nameElement.append(nameOption)
+            }
+        }
+    }
+
+    updateChannelSelect(){
+        let manufacturer = this.patchElementBottom.querySelector("#manufacturer").value
+        let name = this.patchElementBottom.querySelector("#name").value
+        let channelElement = this.patchElementBottom.querySelector("#channel")
+        channelElement.innerHTML = ""
+        for(let i=0;i<fixtureManager.fixtureLibrary.length;i++){
+            let fixture = fixtureManager.fixtureLibrary[i]
+            if(fixture.manufacturer == manufacturer && fixture.name == name){
+                let channelModes = Object.keys(fixture.modes)
+                for(let i=0;i<channelModes.length;i++){
+                    let channel = channelModes[i]
+                    let channelOption = document.createElement('option')
+                    channelOption.value = channel
+                    channelOption.innerHTML = channel
+                    channelElement.append(channelOption)
+                }
+            }
+        }
     }
 }
