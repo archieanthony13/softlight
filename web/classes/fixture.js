48475af9222407244071072ba489c72c8908b329
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2025-12-08 12:50:19 +0000

    Created functions to allow for manual fixture control which will override data from cues and sequences

--- web/classes/fixture.js
+++ web/classes/fixture.js
@@ -5,6 +5,7 @@ class Fixture{
         this.mode = mode
         this.fixtureProfile = fixtureProfile
         this.channelNames = this.fixtureProfile.modes[this.mode]
+        this.manualChannels = new Array(this.channelNames.length).fill(false)
         this.channels = []
         for(let i=0;i<this.channelNames.length;i++){
             this.channels.push(this.fixtureProfile.channels[this.channelNames[i]].defaultValue)
@@ -17,5 +18,17 @@ class Fixture{
         } else {
             this.channels[this.channelNames.indexOf(channel)] = value
         }
+    }
+
+    updateFixtureManualChannel(channel, value){
+        if(this.channelNames.indexOf(channel) == -1){
+            console.error("Unable to update channel. This does not exist for this fixture.")
+        } else {
+            this.manualChannels[this.channelNames.indexOf(channel)] = value
+        }
+    }
+
+    clearManualChannels(){
+        this.manualChannels.fill(false)
     }
 }
