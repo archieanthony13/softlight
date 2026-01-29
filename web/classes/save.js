80e1cde543c7ea67e5da592fa5bad8548a205e83
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2026-01-29 14:43:35 +0000

    Created Save class which will eventually handle all file saving and loading

---
+++ web/classes/save.js
@@ -0,0 +1,25 @@
+class Save{
+    constructor(){
+        this.data = {}
+    }
+
+    generateSaveData(){
+
+    }
+
+    saveToFile(){
+
+    }
+
+    saveToBrowser(){
+
+    }
+
+    loadFromFile(){
+
+    }
+
+    loadFromBrowser(){
+        
+    }
+}
