b8715b773f96371e393590fd01bbb6833839b19f
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2026-02-11 09:29:47 +0000

    Able to save palettes and load them back correctly

--- web/classes/save.js
+++ web/classes/save.js
@@ -1,6 +1,6 @@
 class Save{
     constructor(){
-        this.data = {"fixtures":{},"sequences":{},"data":{}}
+        this.data = {"fixtures":{},"sequences":{},"palettes":{"dimmer":{},"color":{},"position":{},"beam":{},"shape":{}},"data":{}}
 
         // window.onbeforeunload = function(e){
         //     e.preventDefault()
@@ -8,7 +8,7 @@ class Save{
     }
 
     generateSaveData(){
-        this.data = {"fixtures":{},"sequences":{},"palettes":{},"data":{}}
+        this.data = {"fixtures":{},"sequences":{},"palettes":{"dimmer":{},"color":{},"position":{},"beam":{},"shape":{}},"data":{}}
         let fixtures = fixtureManager.fixtures
         for(let i=0;i<fixtures.length;i++){
             let fixture = fixtures[i]
@@ -79,7 +79,7 @@ class Save{
         fixtureManager.fixtures = []
         fixtureManager.selectedFixtures = []
         sequenceManager.sequences = {}
-        paletteManager.palettes = {}
+        paletteManager.palettes = {"dimmer":{},"color":{},"position":{},"beam":{},"shape":{}}
     }
 
     load(){
